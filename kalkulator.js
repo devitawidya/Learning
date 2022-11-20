797e35773d249159576d583a5cc4fe7ddc05eea2
devitawidya <contact.devitawidya@gmail.com>
2022-11-20 21:19:00 +0700

    add gitignore

--- kalkulator.js
+++ kalkulator.js
@@ -6,4 +6,3 @@ var a = prompt('Input the first number : ');
 var b = prompt('Input the second number : ');
 var c = Number(a) + Number(b);
 console.log('The result is '+c)
-console.log('thank you for using this calculator.')
