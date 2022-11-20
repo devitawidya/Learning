892cfd68be9f3f485b48ab2dd3348577db10939e
devitawidya <contact.devitawidya@gmail.com>
2022-11-20 20:59:46 +0700

    add thank you message

--- kalkulator.js
+++ kalkulator.js
@@ -6,3 +6,4 @@ var a = prompt('Input the first number : ');
 var b = prompt('Input the second number : ');
 var c = Number(a) + Number(b);
 console.log('The result is '+c)
+console.log('thank you for using this calculator.')
